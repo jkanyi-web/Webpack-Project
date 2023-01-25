16358dea9e157286fff41acc667de8044f0f7701
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-01-25 22:05:00 +0300

    Further fixes to enhance webpack experience and functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   mode: 'development',
@@ -14,6 +13,9 @@ module.exports = {
     assetModuleFilename: '[name][ext]',
   },
   devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist'),
@@ -56,6 +58,5 @@ module.exports = {
       filename: 'index.html',
       template: 'src/index.html',
     }),
-    new BundleAnalyzerPlugin(),
   ],
 }
